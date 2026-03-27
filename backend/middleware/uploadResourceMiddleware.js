040daf66e88244cc030c18333901c99567bc5b33
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-28 04:29:35 +0500

    mizuka reousrce cloudinary now public access

--- backend/middleware/uploadResourceMiddleware.js
+++ backend/middleware/uploadResourceMiddleware.js
@@ -12,7 +12,8 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
 	cloudinary: cloudinary,
 	params: {
-		folder: 'mizuka_resources', // separate folder for resources
+		folder: 'mizuka_resources',
+		access_mode: 'public', // separate folder for resources
 		resource_type: 'auto', // Cloudinary auto-detects file type
 		// limiting formats for now atleast to common types, can expand later if needed:
 		// allowed_formats: ['jpg', 'png', 'jpeg', 'webp', 'avif', 'gif', 'pdf', 'doc', 'docx', 'txt', 'rtf', 'odt', 'ppt', 'pptx', 'mp4', 'mov', 'avi', 'mkv', 'mp3', 'wav', 'zip'],
