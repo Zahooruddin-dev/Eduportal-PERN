c021ca57d31289dfc6af669f72d9fe2498a266f7
zahooruddin-dev <mzkhan886@gmail.com>
2026-03-28 04:03:24 +0500

    New resrouces routes mounted to the server

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-
 const classRoutes = require('./routes/classes');
 const enrollRoutes = require('./routes/enrollRoutes');
 const authRoutes = require('./routes/authRoutes');
 const announcementRoutes = require('./routes/announcements');
 const attendanceRoutes = require('./routes/attendanceRoutes');
+const resourceRoutes = require('./routes/classResources');
 
 const app = express();
 
@@ -17,6 +17,7 @@ app.use('/api/class', classRoutes);
 app.use('/api/class/:classId/announcements', announcementRoutes);
 app.use('/api/attendance', attendanceRoutes);
 app.use('/api/announcements', announcementRoutes);
+app.use('/api/class/:classId/resources', resourceRoutes);
 app.use('/api/enroll', enrollRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/uploads', express.static('uploads'));
@@ -24,7 +25,7 @@ app.use('/uploads', express.static('uploads'));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
