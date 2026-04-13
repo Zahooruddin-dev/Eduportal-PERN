96c36fa2ce7ed4d0cf5785e8cfe4c2976875e670
zahooruddin-dev <mzkhan886@gmail.com>
2026-04-13 19:59:45 +0500

    Removed unused import of spinner in student annoucemtns

--- frontend/src/components/Sidebar/Tabs/StudentTabs/StudentAnnouncements/StudentAnnouncements.jsx
+++ frontend/src/components/Sidebar/Tabs/StudentTabs/StudentAnnouncements/StudentAnnouncements.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { getMyAnnouncements } from '../../../../../api/api';
-import { SpinnerIcon } from '../../../../Icons/Icon';
 
 export default function StudentAnnouncements() {
   const [announcements, setAnnouncements] = useState([]);
